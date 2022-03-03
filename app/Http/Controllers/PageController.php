47d207ae7696fdcd7c6c390b164e91f7fe266422
judamtar67 <judamalmukhtar72@gmail.com>
2022-03-03 16:59:39 +0700

    Kode Program 2

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    //
+    public function index(){
+        return "Selamat Datang";
+    }
+    public function about(){
+        return "Jud Amal Mukhtar, 2041720168";
+    }
+    public function articles($id){
+        return "Halaman Artikel dengan Id {$id}";
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [App\Http\Controllers\PageController::class, 'index']);
+Route::get('/about', [App\Http\Controllers\PageController::class, 'about']);
+Route::get('/articles/{id}', [App\Http\Controllers\PageController::class, 'articles']);
