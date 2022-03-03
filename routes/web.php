e7d1332d667e2441fa5b5c07841ca99cd4b90219
judamtar67 <judamalmukhtar72@gmail.com>
2022-03-03 17:08:27 +0700

    Kode Program 3

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [App\Http\Controllers\PageController::class, 'index']);
-Route::get('/about', [App\Http\Controllers\PageController::class, 'about']);
-Route::get('/articles/{id}', [App\Http\Controllers\PageController::class, 'articles']);
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index']);
+Route::get('/about', [App\Http\Controllers\AboutController::class, 'about']);
+Route::get('/articles/{id}', [App\Http\Controllers\ArticleController::class, 'articles']);
